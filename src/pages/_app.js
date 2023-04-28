3920c4122bf951d6f9f5f8e1786bb3beacc0a0e0
harshblip <harshjipkate@gmail.com>
2023-04-28 10:16:15 +0530

    commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,16 @@
 import "@/styles/globals.css";
 import "@/styles/homepage.css";
 import Navbar from "../components/Navbar";
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
 
   return(
-<>
+<main className = {inter.className}>
 <Navbar />
 <Component {...pageProps} />
-</>
+</main>
     )
 }
